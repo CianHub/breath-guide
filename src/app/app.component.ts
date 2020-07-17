929055453621282907388d838e4b89ae2638f738
Cian O'Gralaigh <ihatecian@gmail.com>
2020-07-17 17:27:33 +0100

    Setup application, add bg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'breath';
+  @ViewChild('breath-indicator') breathIndicator: ElementRef;
 }
