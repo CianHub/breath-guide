104fe48404d03487ca16638f53e6dc9a7f1020c1
Cian O'Gralaigh <ihatecian@gmail.com>
2020-07-17 18:06:56 +0100

    Add basic styling and functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,46 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'breath';
-  @ViewChild('breath-indicator') breathIndicator: ElementRef;
+
+  totalTime = 19000;
+  inhaleTime = 4000;
+  holdTime = 7000;
+
+  inhale: boolean;
+  exhale: boolean;
+  hold: boolean;
+
+  breathText: string;
+
+  ngOnInit() {
+    this.breathAnimation();
+    setInterval(this.breathAnimation, this.totalTime);
+  }
+
+  breathAnimation = () => {
+    this.breathText = 'Inhale';
+    this.inhale = true;
+    this.exhale = false;
+
+    setTimeout(() => {
+      this.breathText = 'Hold';
+
+      setTimeout(() => {
+        this.breathText = 'Exhale';
+        this.inhale = false;
+        this.exhale = true;
+      }, this.holdTime);
+    }, this.inhaleTime);
+  };
+
+  handleSize = (): string => {
+    if (this.inhale) return 'grow';
+    else if (this.exhale) return 'shrink';
+  };
 }
